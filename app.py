8eb07601be869d6d06f2c864658b98df2c4586e3
loganr0217 <lrichards0217@gmail.com>
2024-11-24 09:43:05 -0500

    Full response endpoint and chat history

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import ChatOpenAI
 from langchain_pinecone import PineconeVectorStore
-from langchain.chains import create_retrieval_chain
+from langchain.chains import create_retrieval_chain, create_history_aware_retriever
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 # Importing langsmith Client for communication
 from langsmith import Client
@@ -28,15 +28,16 @@ from mySecrets import Secrets
 # Setting up pinecone index
 from pinecone import Pinecone, ServerlessSpec
 
+# Setting up OpenAPI key, PineCone API, LangChain API
+os.environ["OPENAI_API_KEY"] = Secrets.OPENAI_API_KEY
+os.environ['PINECONE_API_KEY'] = Secrets.PINECONE_API_KEY
+os.environ["LANGCHAIN_API_KEY"] = Secrets.LANGCHAIN_API_KEY
+
 # Initializing client and flask app
 client = Client()
 app = Flask(__name__)
 CORS(app)
 
-# Setting up OpenAPI key, PineCone API, LangChain API
-os.environ["OPENAI_API_KEY"] = Secrets.OPENAI_API_KEY
-os.environ['PINECONE_API_KEY'] = Secrets.PINECONE_API_KEY
-os.environ["LANGCHAIN_API_KEY"] = Secrets.LANGCHAIN_API_KEY
 
 # Choosing llm model --> using 4o-mini 
 llm = ChatOpenAI(model="gpt-4o-mini")
@@ -52,6 +53,23 @@ retriever = vectorstore.as_retriever(
     search_kwargs={"k": 5, "fetch_k": 5, "lambda_mult": 0.5},
 )
 
+# Creates new question using previous ones
+system_prompt_with_history = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+prompt_with_history = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt_with_history),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{input}"),
+    ]
+)
+# Retriever with history
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, prompt_with_history
+)
+
 # Using custom prompt to get property info
 system_prompt = (
     "You are given a list of documents (each associated with a property) and their associated descriptions, titles, and other information as context. "
@@ -65,21 +83,23 @@ system_prompt = (
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
+        MessagesPlaceholder(variable_name="chat_history"),
         ("human", "{input}"),
     ]
 )
 
 # Creating chain so we can grab source from context
 question_answer_chain = create_stuff_documents_chain(llm, prompt)
-rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
 
+chat_history = []
 
 # Endpoint to ask question to chatbot
 @app.route("/ask")
 def askQuestion():
     question = request.args.get('question', default='null', type=str)
 
-    response = rag_chain.invoke({"input": question})
+    response = rag_chain.invoke({"input": question, "chat_history": chat_history})
 
     # Formatting answer so it can be returned
     fullAnswer = response["answer"] + "\n\nYou can check out more related properties here: \n"
@@ -87,6 +107,24 @@ def askQuestion():
         fullAnswer += found.metadata["title"] + ": " + found.metadata["source"] + "\n"
 
     return fullAnswer
+
+# Endpoint to ask question to chatbot and get answer, context, titles
+@app.route("/askFull")
+def askQuestionFullResponse():
+    question = request.args.get('question', default='null', type=str)
+
+    # Using rag_chain to get answer and context
+    response = rag_chain.invoke({"input": question, "chat_history": chat_history})
+
+    currentContext = [str(i) for i in response["context"]]
+    currentTitles = [i.metadata["title"] for i in response["context"]]
+
+    # Formatting answer so it can be returned
+    fullAnswer = response["answer"] + "\n\nYou can check out more related properties here: \n"
+    for found in response["context"]:
+        fullAnswer += found.metadata["title"] + ": " + found.metadata["source"] + "\n"
+
+    return [fullAnswer, currentContext, currentTitles]
 
 
 
